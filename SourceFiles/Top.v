69ac83751d627c3a8c2c826fca03ebc5c5d492e0
JosueRodriguez <josue.a.rodriguez@ttu.edu>
2018-02-08 17:21:33 -0600

    Removed test bench from SourceFiles folder, Added TOP to source files, unable to test so ports are assumed

---
+++ SourceFiles/Top.v
@@ -0,0 +1,5 @@
+module top()
+        frequency f1(JA, clk, an, seg);
+ //was not able to implement, JA is PMOD, clk is system clock, an is anode number that controls digit, seg is segment pattern
+ //Cannot test this
+endmodule
